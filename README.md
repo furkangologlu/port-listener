be5fe9d420bc816e874789482e67d5af5cd9a4b5
Furkan <gologlufurkan484@gmail.com>
2021-01-28 00:01:20 +0300

    updates

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 # port-listener
 
-### installation
+  
+  ### installation
 ```bash
-npm install-g port-listener
+npm install -g port-listener
+#for linux:
+sudo npm install -g port-listener
 ```
+
 You can install the module on your computer using this command.
 
+  
+---
 ### usage
+
 ```bash
-port-listener <port>
-#example: port-listener 8000
+port-listener <options>
 ```
+ options:
+|parameter|description|required|default value|
+|--|--|--|--|
+|-e|encoding|false|utf-8
+|-p|port|true|-|
+|-h|help|-|-|
+---
+### Last Updates
+
+ - coloruful information
+ - parameterize usage
+ 
+
+If you have a problem, don't forget to open new issue on github  <3
--- app.js
+++ app.js
@@ -1,39 +1,63 @@
+#!/usr/bin/env node
 const net = require('net');
+const program = require('commander');
+
+// const program = new commander.Command()
+
+
+
+
+
+program
+    .option("-p, --port <port>", "Port to listen on", parseInt)
+    .option("-e, --encoding <fmt>", "Encoding (\"utf8\" is default)")
+    .parse(process.argv)
+// .version    (package.version)
+// .description(package.description)
 
 const server = new net.Server()
 
-const port = process.argv[2]
-if(!port) {
-    if(!port) console.error('Please enter port, for example: port-listener 5000')
+
+const { port, encoding } = program.opts()
+if (!port) {
+    console.log(program.helpInformation())
+    // console.error('Please enter port, for example: port-listener 5000')
     process.exit(1)
 }
 
 
-server.on('connection' , connection => {
+server.on('connection', connection => {
     const ip = connection.remoteAddress.substr(7)
     const port = connection.remotePort
-    
-    console.log('[INFO]' , `New connection from ${ip}:${port}`)
 
-    connection.on('data' , bufData => {
-        const data = bufData.toString()
-        console.log(`[DATA] : ${ip} -> ${bufData.byteLength} bytes`)
+    console.log('\x1b[36m%s\x1b[0m', '[INFO]', `New connection from ${ip}:${port}`)
+
+    connection.on('data', bufData => {
+        const data = bufData.toString(encoding || 'utf-8')
+        console.log('\x1b[32m%s\x1b[0m', '[DATA]', `: ${ip} -> ${bufData.byteLength} bytes`)
         console.log(data)
     })
-    
-    connection.on('close' , () => {
-        console.log('[INFO]' , `Disconnected  ${ip == "" ? '127.0.0.1' : ip}`)
+
+    connection.on('close', () => {
+        console.log('\x1b[36m%s\x1b[0m', '[INFO]', `Disconnected  ${ip == "" ? '127.0.0.1' : ip}`)
     })
 })
 
 
 
 try {
-    server.listen(port , () => {
+    server.listen(port, () => {
         const interfaces = require('os').networkInterfaces()['Wi-Fi']
         const locaIP = interfaces.find(interface => interface.family === 'IPv4').address
         console.log(`Server Listening at ${locaIP}:${port}`)
-    } )
+    })
 } catch (error) {
-    console.log(error.message)
+    console.log(error.name)
 }
+
+
+
+
+
+
+
